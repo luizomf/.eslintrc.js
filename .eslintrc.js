45034a7cd1be66d5be5345e4370ab534eba2b876
Luiz Otávio <todoespacoonline@gmail.com>
2019-12-30 10:43:48 -0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-filename-extension': 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+};
